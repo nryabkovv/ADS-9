635b76fb84de61a8dea3b08658c52b11bcae4137
nryabkov <78977205+nryabkovv@users.noreply.github.com>
2023-06-03 23:11:19 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,25 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
-
-#endif  // INCLUDE_BST_H_
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "bst.h"
+BST<std::string> makeTree(const char* filename) {
+    BST<std::string> tree;
+    std::string word;
+    std::ifstream f(filename);
+    if (!f)
+	 std::cout << "error" << std::endl;
+        std::cout << "error" << std::endl;
+    while (!f.eof()) {
+        char ch = tolower(f.get());
+        if (isalpha(ch)) {
+            word += ch;
+        } else {
+            tree.add(word);
+            word.clear();
+        }
+    }
+    f.close();
+    return tree;
+}
